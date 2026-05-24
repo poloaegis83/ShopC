48425f7d204845dd72509937ddb8f150df57f699
Longhao Lee <poloaegis83@gmail.com>
2026-05-24 23:29:37 +0800

    版號 10524

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 35
         versionCode = 4
-        versionName = "10522"
+        versionName = "10524"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
